9af3d6cbd9a693d869dce01b494795d70e12820c
woyeah <66942709+woyeah@users.noreply.github.com>
2021-09-06 01:56:23 +0800

    hello world

---
+++ src/main/java/com/olympus/zeus/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.olympus.zeus;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String say(){
+        return "Hello World";
+    }
+}
